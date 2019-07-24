1d56694800b7b7380d662d4ffc22181b990a42fc
Christopher Caldwell <christophercaldwell@IT-33756.attlocal.net>
2019-07-24 03:47:40 -0500

    included husky git hooks

---
+++ api/git-hooks/testing-hooks.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+printf "\n\nHey!"
+printf "\n\n"
+
+# Cool idea of running hook to upload swagger whenever the template is committed.
+#  Implement in personal projects, cant determine how to protect in open source
+
+# huskyrc
+# git show | grep swagger
