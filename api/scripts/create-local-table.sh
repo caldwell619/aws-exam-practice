513af3bbf4585360c7b14be7d3beb040b0af58da
Christopher Caldwell <christophercaldwell@IT-33756.attlocal.net>
2019-07-24 06:23:16 -0500

    changing table design

--- api/scripts/create-local-table.sh
+++ api/scripts/create-local-table.sh
@@ -4,13 +4,8 @@ source .env
 aws dynamodb create-table \
   --table-name $TABLE_NAME \
   --attribute-definitions \
-      AttributeName=User,AttributeType=S \
-      AttributeName=BusinessDescription,AttributeType=S \
-      AttributeName=BusinessEvent,AttributeType=S \
-      AttributeName=EventDescription,AttributeType=S \
-      AttributeName=PetCheckoutDate,AttributeType=S \
+      AttributeName=Identifier,AttributeType=S \
+      AttributeName=Description,AttributeType=S \
   --endpoint-url http://localhost:8000 \
-  --key-schema AttributeName=User,KeyType=HASH AttributeName=BusinessDescription,KeyType=RANGE \
+  --key-schema AttributeName=Identifier,KeyType=HASH AttributeName=Description,KeyType=RANGE \
   --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
-  --global-secondary-indexes IndexName=BusinessEvents,KeySchema=["{AttributeName=BusinessEvent,KeyType=HASH}","{AttributeName=EventDescription,KeyType=RANGE}"],Projection="{ProjectionType=ALL}",ProvisionedThroughput="{ReadCapacityUnits=5,WriteCapacityUnits=5}" \
-  --local-secondary-indexes IndexName=CheckOutDate,KeySchema=["{AttributeName=User,KeyType=HASH}","{AttributeName=PetCheckoutDate,KeyType=RANGE}"],Projection="{ProjectionType=ALL}"
