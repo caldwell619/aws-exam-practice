4244514df98b4c07c9ca89d41adb5a7b3742172d
Christopher Caldwell <christophercaldwell@IT-33756.attlocal.net>
2019-07-24 06:24:33 -0500

    finishing login

--- api/src/lambdas/user/login/index.js
+++ api/src/lambdas/user/login/index.js
@@ -1,5 +1,7 @@
 const DynamoDB = require("aws-sdk/clients/dynamodb");
+const secretsmanagerSDK = require("aws-sdk/clients/secretsmanager");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 let options = {
 	region: "us-east-1"
 };
@@ -7,42 +9,86 @@ if (process.env.AWS_SAM_LOCAL) {
 	options.endpoint = "http://host.docker.internal:8000";
 }
 const docClient = new DynamoDB.DocumentClient(options);
-
+const secretsManager = new secretsmanagerSDK();
 const headers = {
 	"Access-Control-Allow-Headers": "*",
 	"Access-Control-Allow-Origin": "*",
 	"Access-Control-Allow-Methods": "*",
 	"Content-Type": "application/json"
 };
+const TableName = process.env.TABLE_NAME;
+const SigningKeyID = process.env.SIGNING_KEY_NAME;
+const errorResponse = {
+	headers,
+	statusCode: 402,
+	body: "That combination does not match our records"
+};
+const secretsParams = {
+	SecretId: SigningKeyID
+};
 
 exports.handler = async event => {
-	const { Identifier, PlainTextPassword, Description } = JSON.parse(event.body);
-	const params = {
-		TableName: "Dev-Furry-Friends",
-		KeyConditionExpression: "User = :usr and BusinessDescription = :bsd",
-		ExpressionAttributeValues: {
-			":usr": Username,
-			":bsd": `${BusinessId}_employee`
-		}
-	};
+	const { Identifier, PlainTextPassword } = JSON.parse(event.body);
+	const params = defineParams(Identifier);
 	try {
 		const dynamoReturnedUser = await docClient.query(params).promise();
-		const validation = await bcrypt.compare(
+		const isValidLogin = await determineValidLogin(
 			PlainTextPassword,
-			dynamoReturnedUser.Password
+			dynamoReturnedUser.Items[0].Password
 		);
-		console.log("Validation: ", validation);
-		return {
-			headers,
-			statusCode: 200,
-			body: "Yay!"
-		};
+		if (isValidLogin) {
+			delete dynamoReturnedUser.Items[0].Password;
+			const token = await generateToken(secretsParams, Identifier);
+			return formatPositiveResponse(dynamoReturnedUser, token);
+		} else {
+			return errorResponse;
+		}
 	} catch (error) {
-		console.log("error: ", error);
-		return {
-			headers,
-			statusCode: 402,
-			body: "nooooo"
-		};
+		console.log("inside of the error: ", error);
+		return errorResponse;
 	}
 };
+
+const getSigningKey = async secretsParams => {
+	const secretsManagerResponse = await secretsManager
+		.getSecretValue(secretsParams)
+		.promise();
+	return JSON.parse(secretsManagerResponse.SecretString).signingKey;
+};
+
+const defineParams = Identifier => ({
+	TableName,
+	KeyConditionExpression: "Identifier = :usr and Description = :bsd",
+	ExpressionAttributeValues: {
+		":usr": Identifier,
+		":bsd": "user_profile"
+	}
+});
+
+const determineValidLogin = async (PlainTextPassword, dbUserPassword) => {
+	const validation = await bcrypt.compare(PlainTextPassword, dbUserPassword);
+	return validation;
+};
+
+const generateToken = async (secretsParams, Identifier) => {
+	const signingKey = await getSigningKey(secretsParams);
+	return await jwt.sign({ Identifier }, signingKey, {
+		expiresIn: "24h"
+	});
+};
+
+const formatPositiveResponse = (dynamoReturnedUser, token) => ({
+	headers,
+	statusCode: 200,
+	body: JSON.stringify({
+		...dynamoReturnedUser.Items[0],
+		token,
+		role: "user"
+	})
+});
+
+exports.getSigningKey = getSigningKey;
+exports.defineParams = defineParams;
+exports.determineValidLogin = determineValidLogin;
+exports.generateToken = generateToken;
+exports.formatPositiveResponse = formatPositiveResponse;
--- api/src/lambdas/user/register/index.js
+++ api/src/lambdas/user/register/index.js
@@ -1,4 +1,5 @@
 const DynamoDB = require("aws-sdk/clients/dynamodb");
+const bcyrpt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 let options = {
 	region: "us-east-1"
@@ -13,34 +14,49 @@ const headers = {
 	"Access-Control-Allow-Methods": "*",
 	"Content-Type": "application/json"
 };
+const TableName = process.env.TABLE_NAME;
+const signingKey = process.env.SIGNING_KEY;
 
 exports.handler = async event => {
-	console.log("body is: ", event.body);
-	return {
-		headers,
-		statusCode: 200,
-		body: "done"
+	const { Identifier, Password, FirstName, LastName } = JSON.parse(event.body);
+	const hashedPassword = await bcyrpt.hash(Password, 10);
+	const params = {
+		TableName,
+		Item: {
+			Identifier,
+			Description: "user_profile",
+			Password: hashedPassword,
+			FirstName,
+			LastName
+		}
 	};
-	// const params = {
-	// 	TableName: "Users",
-	// 	Key: {
-	// 		Username,
-	// 		BusinessId
-	// 	}
-	// };
 
-	// try {
-	// 	const user = await docClient.put(params).promise();
-	// 	return {
-	// 		headers,
-	// 		body: JSON.stringify(user),
-	// 		statusCode: 200
-	// 	};
-	// } catch (error) {
-	// 	return {
-	// 		headers,
-	// 		body: "error",
-	// 		statusCode: 400
-	// 	};
-	// }
+	try {
+		const token = await jwt.sign(
+			{
+				Identifier
+			},
+			signingKey,
+			{ expiresIn: "24h" }
+		);
+		await docClient.put(params).promise();
+		return {
+			headers,
+			body: JSON.stringify({
+				Identifier,
+				FirstName,
+				LastName,
+				token,
+				role: "user"
+			}),
+			statusCode: 200
+		};
+	} catch (error) {
+		console.log("error: ", error);
+		return {
+			headers,
+			body: "error",
+			statusCode: 400
+		};
+	}
 };
