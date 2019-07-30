00fc65602bbdb51ec569230879547d8638165297
Christopher Caldwell <christophercaldwell@IT-33756.attlocal.net>
2019-07-29 21:35:58 -0500

    messing with hooks

--- git-hooks/testing-hooks.sh
+++ git-hooks/testing-hooks.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-printf "\n\nHey!"
-printf "\n\n"
+git show | grep ui/
 
-# Cool idea of running hook to upload swagger whenever the template is committed.
-#  Implement in personal projects, cant determine how to protect in open source
-
-# huskyrc
-# git show | grep swagger
+if [ $? == 0 ]
+then
+  echo "true"
+else
+  echo "false"
+fi
