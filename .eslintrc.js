a3c821daacd5cab6800fe24aa522da6b873002f3
caldwell619 <caldwell8814@gmail.com>
2020-02-19 14:12:08 -0600

    centralizing eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
+const isRunningLocally = process.env.NODE_ENV !== 'production'
+
 module.exports = {
 	root: true,
 	env: {
-		node: true
+    node: true,
+    es6: true
 	},
-	extends: ['plugin:vue/essential', 'eslint:recommended'],
+	extends: ['eslint:recommended'],
 	rules: {
-    'no-console': 'warn',
-    'no-debugger': 'warn',
-    'no-undef': 'warn',
+    'no-console': isRunningLocally ? 'off' : 'error',
     // always use single quotes
     quotes: [2, 'single', { avoidEscape: true }],
     // errors when semi colon are used to close
@@ -16,17 +17,6 @@ module.exports = {
     'comma-dangle': [0, 'always']
   },
 	parserOptions: {
-		parser: 'babel-eslint'
-	},
-	overrides: [
-		{
-			files: [
-				'**/__tests__/*.{j,t}s?(x)',
-				'**/tests/unit/**/*.spec.{j,t}s?(x)'
-			],
-			env: {
-				jest: true
-			}
-		}
-	]
+    ecmaVersion: 10
+	}
 }
