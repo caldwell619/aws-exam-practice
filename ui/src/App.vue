db301e2f0a3be41902ac73e3afc7f76a96ff174c
caldwell619 <caldwell8814@gmail.com>
2020-02-20 09:45:55 -0600

    check on if app ready

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -2,16 +2,26 @@
   v-app
     Header
     v-content
-      router-view
+      router-view(v-if="isReadyToShow")
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
 import Header from '@/components/header/Header.vue'
 
 export default {
   components: {
     Header
-  }
+	},
+	computed: {
+		...mapGetters('session', ['isReadyToShow'])
+	},
+	methods: {
+		...mapActions('session', ['restoreSessionFromLocalStorage'])
+	},
+	async mounted(){
+		await this.restoreSessionFromLocalStorage()
+	}
 }
 </script>
 
